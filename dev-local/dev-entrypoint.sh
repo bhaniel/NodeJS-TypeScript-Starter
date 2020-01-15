5b461117d88728e8179bc6fd7930267a18449a2b
Haniel Bitton <Haniel.Bitton@888holdings.com>
2020-01-15 15:21:33 +0200

    removed the while loop in the bash file

--- dev-local/dev-entrypoint.sh
+++ dev-local/dev-entrypoint.sh
@@ -6,20 +6,17 @@ yarn
 # npm run debug
 # npm run dev
 
-echo "Docker environment started."
-while :
-do
-	echo ""
-	echo "Open a new terminal and You can use:"
-	echo ""
-	echo "- npm run docker:bash"
-	echo "- npm run docker:watch"
-	echo "- npm run docker:watch:debug"
-	echo "- npm run docker:test"
-	echo "- npm run docker:test:debug"
-	echo "- npm run docker:test:e2e"
-	echo "- npm run docker:test:e2e:debug"
-	echo ""
-	echo "Press [CTRL+C] to stop.."
-	sleep 60
-done
+echo ""
+echo "Open a new terminal and You can use:"
+echo ""
+echo "- npm run docker:bash"
+echo "- npm run docker:watch"
+echo "- npm run docker:watch:debug"
+echo "- npm run docker:test"
+echo "- npm run docker:test:debug"
+echo "- npm run docker:test:e2e"
+echo "- npm run docker:test:e2e:debug"
+echo ""
+echo "Press [CTRL+C] to stop.."
+#this command will kep our continer a live
+tail -f /dev/null
